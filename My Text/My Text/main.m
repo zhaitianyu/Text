d53532509d6b366a7fe2c850b542b867d7160ab8
翟 <18738781707@163.com>
2015-11-25 19:31:09 +0800

    初始化项目

---
+++ My Text/My Text/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Text
+//
+//  Created by 翟天宇 on 15/11/25.
+//  Copyright © 2015年 翟天宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
